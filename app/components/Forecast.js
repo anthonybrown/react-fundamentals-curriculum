ba6df2975ec7ad2feb71779ad2c09109a7ed873e
Tony Brown <tony@tonybrown.me>
2017-06-12 12:41:27 -0400

    create helper functions

--- app/components/Forecast.js
+++ app/components/Forecast.js
@@ -1,9 +1,71 @@
 import React, { Component } from 'react'
+import api from '../utils/api'
+import queryString from 'query-string'
+import utils, { getDate, convertTemp } from '../utils/helpers'
+
+const DayItem = (props) => {
+  let date = getDate(props.day.dt)
+  let icon = props.day.weather[0].icon
+  return (
+    <div className='dayContainer'>
+      <img
+        src={'./app/images/weather-icons/' + icon + '.svg'}
+        alt='weather'
+        className='weather'
+      />
+      <h2 className='subHeader'>{date}</h2>
+    </div>
+  )
+}
 
 class Forecast extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      forecastData: [],
+      loading: true
+    }
+
+    this.makeRequest = this.makeRequest.bind(this)
+  }
+
+  componentDidMount() {
+    this.city = queryString.parse(this.props.location.search).city
+    this.makeRequest(this.city)
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.city = queryString.parse(nextProps.location.search).city
+  }
+
+  makeRequest(city) {
+    this.setState(() => {
+      return {
+        loading: true
+      }
+    })
+
+    api.getForeCast(city)
+      .then((res) => {
+        this.setState({
+          loading: false,
+          forecastData: res
+        })
+      })
+  }
+
   render() {
     return (
-      <div>Forecast Component</div>
+      this.state.loading === true
+      ? <h1 className='forecast-header'>Loading</h1>
+      : <div>
+        <h1 className='forecast-header'>{this.city}</h1>
+        <div className='forecast-container'>
+          {this.state.forecastData.list.map((listItem) => {
+            return <DayItem key={listItem.dt} day={listItem} />
+          })}
+        </div>
+      </div>
     )
   }
 }
